266a06ec71d8d98383878db6bca29c7535ad0284
KlawitChanyut <Klawitchanyut@gmail.com>
2020-10-13 22:39:26 +0700

    Add code score and hit the doom chibi show score now

--- app/src/main/java/klawit/example/doomguygame/MainActivity.kt
+++ app/src/main/java/klawit/example/doomguygame/MainActivity.kt
@@ -2,10 +2,24 @@ package klawit.example.doomguygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var score : Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        score = 0
+    }
+
+    fun increaseScore(view: View) {
+        score++
+
+        Text_score.text = "Score: " + score
+
     }
 }
