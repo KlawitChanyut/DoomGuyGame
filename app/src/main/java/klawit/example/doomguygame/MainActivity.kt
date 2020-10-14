9335e1c1745a0ecc393c05b796c5831d359942d6
KlawitChanyut <Klawitchanyut@gmail.com>
2020-10-15 00:14:12 +0700

    Add imageArray and create fun hideImges on the MainActivity.kt

--- app/src/main/java/klawit/example/doomguygame/MainActivity.kt
+++ app/src/main/java/klawit/example/doomguygame/MainActivity.kt
@@ -4,17 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     var score : Int = 0
+    var imageArray = ArrayList<ImageView>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         score = 0
+
+        imageArray = arrayListOf(doom1,doom2,doom3,doom4,doom5,doom6,doom7,doom8,doom9)
+
+        hideImages()
 
         object : CountDownTimer(10000,1000) {
             override fun onTick(p0: Long) {
@@ -27,10 +33,16 @@ class MainActivity : AppCompatActivity() {
         }.start()
     }
 
+    fun hideImages() {
+
+        for (image in imageArray) {
+            image.visibility = View.INVISIBLE
+        }
+    }
+
     fun increaseScore(view: View) {
         score++
 
         Text_score.text = "Score: " + score
-
     }
 }
