2265abda6afe6ce663d292d888999cf6b65ff0ec
KlawitChanyut <Klawitchanyut@gmail.com>
2020-10-19 22:16:18 +0700

    Add handler, runnable, random and update onFinish

--- app/src/main/java/klawit/example/doomguygame/MainActivity.kt
+++ app/src/main/java/klawit/example/doomguygame/MainActivity.kt
@@ -3,14 +3,19 @@ package klawit.example.doomguygame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.os.Handler
 import android.view.View
 import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
     var score : Int = 0
     var imageArray = ArrayList<ImageView>()
+    var handler : Handler = Handler()
+    var runnable : Runnable = Runnable {  }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,15 +34,33 @@ class MainActivity : AppCompatActivity() {
 
             override fun onFinish() {
                 Text_time.text = "Time's off"
+                handler.removeCallbacks(runnable)
+                for (image in imageArray) {
+                    image.visibility = View.INVISIBLE
+                }
             }
         }.start()
     }
 
     fun hideImages() {
 
-        for (image in imageArray) {
-            image.visibility = View.INVISIBLE
+        runnable = object : Runnable {
+            override fun run() {
+                for (image in imageArray) {
+                    image.visibility = View.INVISIBLE
+                }
+
+                val random = Random()
+                val index = random.nextInt(8-0)
+                imageArray[index].visibility = View.VISIBLE
+
+                handler.postDelayed(runnable, 500)
+
+            }
         }
+
+        handler.post(runnable)
+
     }
 
     fun increaseScore(view: View) {
